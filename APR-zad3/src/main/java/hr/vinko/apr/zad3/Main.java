845ebb4f828a59da2abe43d85e45c3c19b3405e7
Vinko Kolobara <kolobara.vinko@gmail.com>
2016-12-06 20:05:56 +0100

    Gotovo

---
+++ APR-zad3/src/main/java/hr/vinko/apr/zad3/Main.java
@@ -0,0 +1,190 @@
+package hr.vinko.apr.zad3;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import hr.vinko.apr.zad3.algorithm.Box;
+import hr.vinko.apr.zad3.algorithm.GradientDescent;
+import hr.vinko.apr.zad3.algorithm.NewtonRaphson;
+import hr.vinko.apr.zad3.algorithm.TransformationMixed;
+import hr.vinko.apr.zad3.constraints.ExplicitConstraint;
+import hr.vinko.apr.zad3.constraints.ImplicitConstraint;
+import hr.vinko.apr.zad3.constraints.ImplicitConstraintEquality;
+import hr.vinko.apr.zad3.constraints.ImplicitConstraintInequality;
+import hr.vinko.apr.zad3.function.AbstractFunction;
+import hr.vinko.apr.zad3.function.F1;
+import hr.vinko.apr.zad3.function.F2;
+import hr.vinko.apr.zad3.function.F3;
+import hr.vinko.apr.zad3.function.F4;
+import hr.vinko.apr.zad3.function.IFunction;
+
+public class Main {
+ 
+  public static void main(String[] args) {
+	  
+	  System.setOut(new PrintStream(new OutputStream() {
+
+		     @Override
+		     public void write(int arg0) throws IOException {
+
+		     }
+		  }));
+	  
+	  System.err.println("ZADATAK 1");
+	  zad1();
+	  System.err.println();
+	  System.err.println();
+	  System.err.println();
+	  System.err.println("ZADATAK 2");
+	  zad2();
+	  System.err.println();
+	  System.err.println();
+	  System.err.println();
+	  System.err.println("ZADATAK 3");
+	  zad3();
+	  System.err.println();
+	  System.err.println();
+	  System.err.println();
+	  System.err.println("ZADATAK 4");
+	  zad4();	 
+	  System.err.println();
+	  System.err.println();
+	  System.err.println();
+	  System.err.println("ZADATAK 5");
+	  zad5();
+  }
+
+
+  private static void zad1() {
+    IFunction f3 = new F3();
+    GradientDescent gd1 = new GradientDescent(false, 1e-6, 100);
+    GradientDescent gd2 = new GradientDescent(true, 1e-6, 100);
+
+    double[] x0 = new double[] {0, 0};
+
+    System.err.println("Gradient descent, no line search");
+    System.err.println(Arrays.toString(gd1.solve(x0, f3)));
+    System.err.println();
+    System.err.println("Gradient descent with line search");
+    System.err.println(Arrays.toString(gd2.solve(x0, f3)));
+  }
+
+  private static void zad2() {
+	  AbstractFunction f1 = new F1();
+	  GradientDescent gd = new GradientDescent(true, 1e-6, 100);
+	  NewtonRaphson nr = new NewtonRaphson(true, 1e-6, 100);
+	  
+	  double[] x0 = new double[]{-1.9, 2};
+	  
+	  double[] sol = gd.solve(x0, f1);
+	  
+	  System.err.println("F1");
+	  System.err.println("Gradient Descent");
+	  
+	  System.err.println(Arrays.toString(sol));
+	  System.err.println(f1.getValueCounter() + " " + f1.getDerivationCounter() + " " + f1.getHessianCounter());
+	  f1.clear();
+	  
+	  System.err.println();
+	  System.err.println("Newton Raphson");
+	  
+	  sol = nr.solve(x0, f1);
+	  System.err.println(Arrays.toString(sol));
+	  System.err.println(f1.getValueCounter() + " " + f1.getDerivationCounter() + " " + f1.getHessianCounter());
+	  f1.clear();	  
+	  System.err.println();
+	  
+	  AbstractFunction f2 = new F2();
+	  x0 = new double[] {0.1, 0.3};
+	  System.err.println();
+	  System.err.println("F2");
+	  System.err.println("Gradient Descent");
+	  
+	  sol = gd.solve(x0, f2);
+	  System.err.println(Arrays.toString(sol));
+	  System.err.println(f2.getValueCounter() + " " + f2.getDerivationCounter() + " " + f2.getHessianCounter());
+	  f2.clear();
+	  
+	  System.err.println();
+	  System.err.println("Newton Raphson");
+	  
+	  sol = nr.solve(x0, f2);
+	  System.err.println(Arrays.toString(sol));
+	  System.err.println(f2.getValueCounter() + " " + f2.getDerivationCounter() + " " + f2.getHessianCounter());
+	  f2.clear();	  
+  }
+  
+  private static void zad3() {
+	  AbstractFunction f1 = new F1();
+	  double[] x0 = new double[]{-1.9, 2};
+	  
+	  double[] min = new double[]{-100, -100};
+	  double[] max = new double[]{100, 100};
+	  
+	  ExplicitConstraint expConst = new ExplicitConstraint(min, max);
+	  ImplicitConstraintInequality cons1 = new ImplicitConstraintInequality(x -> x[1] - x[0]);
+	  ImplicitConstraintInequality cons2 = new ImplicitConstraintInequality(x -> 2 - x[0]);
+	  
+	  Box box = new Box(1e-6, 100, 1.3, expConst, Arrays.asList(cons1, cons2));
+	  
+	  double[] sol = box.solve(x0, f1);
+	  
+	  System.err.println("F1");
+	  System.err.println(Arrays.toString(sol));	  
+	  System.err.println();
+	  
+	  AbstractFunction f2 = new F2();
+	  x0 = new double[] {0.1, 0.3};
+
+	  sol = box.solve(x0, f2);
+	  System.err.println("F2");
+	  System.err.println(Arrays.toString(sol));
+  }
+  
+  private static void zad4() {
+	  AbstractFunction f1 = new F1();
+	  double[] x0 = new double[]{-1.9, 2};
+	  
+	  ImplicitConstraintInequality cons1 = new ImplicitConstraintInequality(x -> x[1] - x[0]);
+	  ImplicitConstraintInequality cons2 = new ImplicitConstraintInequality(x -> 2 - x[0]);
+	  
+
+	  TransformationMixed tm = new TransformationMixed(1, 1e-6, new ArrayList<>(), Arrays.asList(cons1, cons2), 100);
+	  
+	  double[] sol = tm.solve(x0, f1);
+	  
+	  System.err.println("F1");
+	  System.err.println(Arrays.toString(sol));	  
+	  System.err.println();
+	  AbstractFunction f2 = new F2();
+	  x0 = new double[] {1, 3};
+
+	  sol = tm.solve(x0, f2);
+	  System.err.println("F2");
+	  System.err.println(Arrays.toString(sol));
+  }
+  
+  private static void zad5() {
+	  AbstractFunction f4 = new F4();
+	  
+	  double[] x0 = new double[] {0, 0};
+	  
+	  ImplicitConstraintInequality cons1 = new ImplicitConstraintInequality(x -> 3 - x[0] - x[1]);
+	  ImplicitConstraintInequality cons2 = new ImplicitConstraintInequality(x -> 3 + 1.5*x[0] - x[1]);
+	  ImplicitConstraintEquality cons3 = new ImplicitConstraintEquality(x -> x[1] - 1);
+	  
+	  TransformationMixed tm = new TransformationMixed(1, 1e-6, Arrays.asList(cons3), Arrays.asList(cons1, cons2), 100);
+	  
+	  double[] sol = tm.solve(x0, f4);
+	  
+	  System.err.println(Arrays.toString(sol));
+	  System.err.println();
+	  x0 = new double[] {5, 5};
+	  System.err.println(Arrays.toString(tm.solve(x0, f4)));
+	  
+  }
+  
+}
---
+++ APR-zad3/src/main/java/hr/vinko/apr/zad3/algorithm/Box.java
@@ -0,0 +1,230 @@
+package hr.vinko.apr.zad3.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import hr.vinko.apr.zad3.constraints.ExplicitConstraint;
+import hr.vinko.apr.zad3.constraints.IConstraint;
+import hr.vinko.apr.zad3.constraints.ImplicitConstraint;
+import hr.vinko.apr.zad3.constraints.ImplicitConstraintInequality;
+import hr.vinko.apr.zad3.function.IFunction;
+
+public class Box implements IAlgorithm {
+
+	private Random rand = new Random();
+
+	private double epsilon;
+	private int iterWithoutProgress;
+	private double alpha;
+	private ExplicitConstraint expConst;
+	private List<ImplicitConstraintInequality> constraints;
+
+	/**
+	 * @param epsilon
+	 * @param iterWithoutProgress
+	 * @param alpha
+	 * @param expConst
+	 * @param constraints
+	 */
+	public Box(double epsilon, int iterWithoutProgress, double alpha, ExplicitConstraint expConst,
+			List<ImplicitConstraintInequality> constraints) {
+		this.epsilon = epsilon;
+		this.iterWithoutProgress = iterWithoutProgress;
+		this.alpha = alpha;
+		this.expConst = expConst;
+		this.constraints = constraints;
+	}
+
+	@Override
+	public double[] solve(double[] x0, IFunction f) {
+
+		if (expConst.check(x0)) {
+			boolean constSatisfied = true;
+			for (IConstraint cons : constraints) {
+				if (!cons.check(x0)) {
+					constSatisfied = false;
+					break;
+				}
+			}
+
+			if (constSatisfied) {
+				List<double[]> simplex = generateSimplex(x0);
+				double[] xC = null;
+				double bestF = Double.MAX_VALUE;
+				int iters = 0;
+				while (true) {
+					
+					Collections.sort(simplex, (x1, x2) -> -Double.compare(f.getValueAt(x1), f.getValueAt(x2)));
+//					simplex.stream().sequential().forEach(x -> System.err.println(f.getValueAt(x)));
+//					System.err.println();
+					xC = calculateCentroid(simplex.subList(1, simplex.size()));
+					double value = f.getValueAt(xC);
+					if (value < bestF) {
+						bestF = value;
+						iters = 0;
+					}
+					iters++;
+
+					if (iters >= iterWithoutProgress) {
+						System.err.println("NO PROGRESS!");
+						break;
+					}
+
+					if (stopCondition(xC, simplex, f))
+						break;
+
+					double[] xR = reflexion(xC, simplex.get(0));
+
+					xR = fixExplicit(xR);
+					xR = fixImplicit(xR, xC);
+					xR = fixWorst(xR, simplex.get(1), xC, f);
+
+					simplex.set(0, xR);
+
+				}
+				return xC;
+			}
+
+		}
+
+		return null;
+	}
+
+	private boolean stopCondition(double[] xC, List<double[]> simplex, IFunction f) {
+		double sum = 0;
+
+		for (double[] x : simplex) {
+			sum += Math.pow(f.getValueAt(xC) - f.getValueAt(x), 2);
+		}
+
+		sum /= simplex.size();
+
+		return Math.sqrt(sum) <= epsilon;
+
+	}
+
+	private double[] fixWorst(double[] xR, double[] xH2, double[] xC, IFunction f) {
+		if (f.getValueAt(xR) > f.getValueAt(xH2)) {
+			xR = moveTowardCentroid(xR, xC);
+		}
+		
+		return xR;
+
+	}
+
+	private double[] fixImplicit(double[] xR, double[] xC) {
+		boolean constSatisfied = false;
+
+		while (!constSatisfied) {
+			constSatisfied = true;
+			for (ImplicitConstraint cons : constraints) {
+				if (!cons.check(xR)) {
+					xR = moveTowardCentroid(xR, xC);
+					constSatisfied = false;
+					break;
+				}
+			}
+		}
+		
+		return xR;
+
+	}
+
+	private double[] fixExplicit(double[] xR) {
+		double[] min = expConst.getMin();
+		double[] max = expConst.getMax();
+
+		for (int i = 0; i < xR.length; i++) {
+			if (xR[i] < min[i])
+				xR[i] = min[i];
+			else if (xR[i] > max[i])
+				xR[i] = max[i];
+		}
+
+		return xR;
+	}
+
+	private double[] reflexion(double[] xC, double[] xH) {
+		double[] xR = new double[xC.length];
+		for (int i = 0; i < xR.length; i++) {
+			xR[i] = (1 + alpha) * xC[i] - alpha * xH[i];
+		}
+
+		return xR;
+
+	}
+
+	private List<double[]> generateSimplex(double[] x0) {
+		int size = 2 * x0.length;
+		List<double[]> simplex = new ArrayList<double[]>(size);
+
+		simplex.add(Arrays.copyOf(x0, x0.length));
+
+		double[] xC = Arrays.copyOf(x0, x0.length);
+
+		double[] min = expConst.getMin();
+		double[] max = expConst.getMax();
+
+		while (simplex.size() < size) {
+
+			double[] newX = new double[xC.length];
+
+			for (int i = 0; i < newX.length; i++) {
+				double r = rand.nextDouble();
+				newX[i] = min[i] + r * (max[i] - min[i]);
+			}
+
+			boolean areSatisfied = false;
+			while (!areSatisfied) {
+
+				areSatisfied = true;
+
+				for (IConstraint cons : constraints) {
+					if (!cons.check(newX)) {
+						areSatisfied = false;
+						break;
+					}
+				}
+
+				newX = moveTowardCentroid(newX, xC);
+			}
+
+			simplex.add(newX);
+			xC = calculateCentroid(simplex);
+		}
+
+		return simplex;
+
+	}
+
+	private double[] moveTowardCentroid(double[] x, double[] xC) {
+
+		double[] ret = new double[x.length];
+
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = 0.5 * (x[i] + xC[i]);
+		}
+		return ret;
+
+	}
+
+	private double[] calculateCentroid(List<double[]> points) {
+
+		double[] xC = new double[points.get(0).length];
+
+		points.forEach(point -> {
+			for (int i = 0; i < xC.length; i++) {
+				xC[i] += point[i];
+			}
+		});
+
+		for (int i = 0; i < xC.length; i++)
+			xC[i] /= (1.0 * points.size());
+
+		return xC;
+	}
+
+}
